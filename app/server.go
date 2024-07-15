70ad4845a53785a4eddba2aa0c30b38d72d92aa2
AbhishekJ <abhishek.jaiswal@windriver.com>
2024-07-15 18:12:26 +0530

    load dot env
    Signed-off-by: AbhishekJ <abhishek.jaiswal@windriver.com>

--- app/server.go
+++ app/server.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v3"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var serveParams = fmt.Sprintf(
+	"%s:%s",
+	os.Getenv("SERVER_HOST"),
+	os.Getenv("SERVER_PORT"),
 )
 
 func main() {
@@ -13,9 +22,10 @@ func main() {
 	// Define a route for the GET method on the root path '/'
 	app.Get("/", func(c fiber.Ctx) error {
 		// Send a string response to the client
+		fmt.Println(os.Getenv("SERVER_PORT"))
 		return c.SendString("Hello, World 👋!")
 	})
 
 	// Start the server on port 3000
-	log.Fatal(app.Listen("0.0.0.0:8000"))
+	log.Fatal(app.Listen(serveParams))
 }
