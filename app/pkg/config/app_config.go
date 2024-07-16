548132045c3e13396182e2465edcaf5214fabd85
Abhishek Jaiswal <abhishek.jaiswal@windriver.com>
2024-07-16 14:55:56 +0530

    updated code
    Signed-off-by: Abhishek Jaiswal <abhishek.jaiswal@windriver.com>

--- app/pkg/config/app_config.go
+++ app/pkg/config/app_config.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/template/html/v2"
 )
 
 // AppConfig func for configuration Fiber app.
@@ -13,9 +14,11 @@ import (
 func AppConfig() fiber.Config {
 	// Define server settings.
 	readTimeoutSecondsCount, _ := strconv.Atoi(os.Getenv("SERVER_READ_TIMEOUT"))
+	engine := html.New("./views", ".html")
 
 	// Return Fiber configuration.
 	return fiber.Config{
 		ReadTimeout: time.Second * time.Duration(readTimeoutSecondsCount),
+		Views:       engine,
 	}
 }
